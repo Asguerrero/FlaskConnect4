75c930786c5042346d62a4777dc63b5a067c8511
asguerrero <valentinaguerrero.as@hotmail.com>
2024-03-27 13:39:39 -0500

    added connect4.py

--- connect4.py
+++ connect4.py
@@ -1,11 +1,38 @@
+'''Connect 4 API implementation: Alex Wcislo, Carlos Flores, Valentina Guerrero'''
+
 import flask
 import json
+import time
 
 app = flask.Flask(__name__)
 
-@app.route('/newgame/player')
-def newgame ():
-    
+allBoards = {}
+- - - - - - -
+- - - - - - -
+- - - - - - -
+- - - - - - -
+- - - - - - -
+- - - - - - -
+@app.route('/newgame/<player>')
+def newgame (player):
+    gameId = int(time.time())
+    state = ["------------------------------------------", player]
+    allBoards[gameId] = state
+    response = {'ID: ': gameId}
+    print(allBoards)
+    return json.dumps(response)
 
+@app.route('nextmove/<gameID>/<oppCol>/<state>')
+def nextmove(gameID, oppCol, state):
+    # Update our local records of the board 
+    # Choose next move 
+    # Update state of the board with our new move
+    updateAllBoards()
+    pickNextMove()
+
+    # Return { 'ID': gameID 'col': myCol 'state' : new_gamestate }
+  
 if __name__ == '__main__':
-    app.run(debug=True)
+    host = 'localhost'
+    port = 5555
+    app.run(host=host, port=port, debug=True)
